caf13be4f01ea0e052f0e6034cbf0cdae053a695
HusseinSerag <husseinserag2014@gmail.com>
2025-06-11 14:28:33 +0200

    refactor: null check for db url

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,8 @@
+import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL env variable is not set");
+}
+const sql = neon(process.env.DATABASE_URL);
+export const db = drizzle(sql);
