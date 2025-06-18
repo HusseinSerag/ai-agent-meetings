fbd3109da47ba937a0a6a37f0492b71d2f79dd3d
HusseinSerag <husseinserag2014@gmail.com>
2025-06-18 17:56:56 +0200

    add: agents page

---
+++ src/app/(dashboard)/agents/page.tsx
@@ -0,0 +1,26 @@
+import { ErrorState } from "@/components/Error";
+import { LoadingState } from "@/components/Loading";
+import {
+  AgentsError,
+  AgentsLoading,
+  AgentsView,
+} from "@/modules/agents/ui/views/agents-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+export default async function AgentsPage() {
+  const queryClient = getQueryClient();
+  // prefetch data on server
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<AgentsLoading />}>
+        <ErrorBoundary fallback={<AgentsError />}>
+          <AgentsView />;
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  );
+}
--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,19 +1,15 @@
 import { auth } from "@/lib/auth";
 import { HomeView } from "@/modules/HomeView";
-import { caller } from "@/trpc/server";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const greeting = await caller.hello({
-    text: "hussein",
-  });
   const session = await auth.api.getSession({
     headers: await headers(),
   });
   if (!session) {
     redirect("/auth/sign-in");
   }
-  return <p>{greeting.greeting}</p>;
+
   return <HomeView />;
 }
