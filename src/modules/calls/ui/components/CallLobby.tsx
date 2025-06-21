f6849b809dd167a4c5fd4b3b6584370404111ce1
HusseinSerag <husseinserag2014@gmail.com>
2025-06-21 23:27:25 +0200

    connect agent

--- src/modules/calls/ui/components/CallLobby.tsx
+++ src/modules/calls/ui/components/CallLobby.tsx
@@ -48,13 +48,14 @@ export function CallLobby({ onJoin }: CallLobbyProps) {
   const { hasBrowserPermission: hasMicPermission } = useMicrophoneState();
   const hasBrowserMediaPermission = hasCameraPermission && hasMicPermission;
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-radial from-sidebar-accent to-sidebar">
-      <div className="flex flex-col items-center justify-center gap-y-6 bg-background rounded-lg p-10 shadow-sm">
+    <div className="flex flex-col items-center w-full justify-center min-h-screen bg-radial from-sidebar-accent to-sidebar">
+      <div className="flex w-full flex-col items-center justify-center gap-y-6 bg-background rounded-lg p-10 shadow-sm">
         <div className="flex flex-col gap-y-2 text-center">
           <h6 className="text-lg font-medium">Ready to join?</h6>
           <p className="text-sm">Set up your call before joining</p>
         </div>
         <VideoPreview
+          className="max-w-full"
           DisabledVideoPreview={
             hasBrowserMediaPermission
               ? DisabledVideoPreview
