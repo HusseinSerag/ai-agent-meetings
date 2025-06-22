c240bad6dd5ad82c7c4eb648588017328390b546
HusseinSerag <husseinserag2014@gmail.com>
2025-06-22 22:15:49 +0200

    add constants

--- src/lib/polar.ts
+++ src/lib/polar.ts
@@ -1,5 +1,5 @@
 import { Polar } from "@polar-sh/sdk";
 export const polarClientInstance = new Polar({
   accessToken: process.env.POLAR_ACCESS_TOKEN,
-  server: "sandbox",
+  server: process.env.NODE_ENV === "production" ? "production" : "sandbox",
 });
